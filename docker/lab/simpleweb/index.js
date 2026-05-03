b125843edbc086c60c9f8043bb7ef7825dfbd7d8
Ajay Tewari <mail.ajaytewari@gmail.com>
2026-05-03 08:13:14 -0700

    leanr docker build

---
+++ docker/lab/simpleweb/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi Ajay! How are you!');
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
